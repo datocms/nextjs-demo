f1b9a2536a2e095f25cc2fff4f7a3c8452329868
Matteo Giaccone <matteo@turnintocoders.it>
2020-03-10 16:07:03 +0100

    Remove node_modules

--- node_modules/next/dist/next-server/server/get-page-files.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type BuildManifest = {
-    devFiles: string[];
-    lowPriorityFiles: string[];
-    pages: {
-        '/_app': string[];
-        [page: string]: string[];
-    };
-};
-export declare function getPageFiles(buildManifest: BuildManifest, page: string): string[];
--- node_modules/next/dist/server/static-paths-worker.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function loadStaticPaths(distDir: string, buildId: string, pathname: string, serverless: boolean): Promise<{
-    paths: string[];
-    fallback: boolean;
-}>;
