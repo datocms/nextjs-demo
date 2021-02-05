7f1908cb4e7634464a97a06154042edafc07110c
Stefano Verna <s.verna@datocms.com>
2021-02-05 09:14:10 +0100

    Upgrade dependencies Use Structured Text field

--- components/post-body.js
+++ components/post-body.js
@@ -1,12 +1,25 @@
-import markdownStyles from './markdown-styles.module.css'
+import { StructuredText, Image } from "react-datocms";
 
 export default function PostBody({ content }) {
   return (
     <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles['markdown']}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
+      <div className="prose prose-lg prose-blue">
+        <StructuredText
+          data={content}
+          renderBlock={({ record }) => {
+            if (record.__typename === "ImageBlockRecord") {
+              return <Image data={record.image.responsiveImage} />;
+            }
+
+            return (
+              <>
+                <p>Don't know how to render a block!</p>
+                <pre>{JSON.stringify(record, null, 2)}</pre>
+              </>
+            );
+          }}
+        />
+      </div>
     </div>
-  )
+  );
 }
