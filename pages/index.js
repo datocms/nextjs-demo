4f31a8d050e823f33a521d43ec9fe6d7c477b1fd
Stefano Verna <s.verna@datocms.com>
2021-02-23 14:49:55 +0100

    Fix

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export async function getStaticProps({ preview }) {
             ...graphqlRequest,
             initialData: await request(graphqlRequest),
             token: process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
-            environment: process.env.NEXT_DATOCMS_ENVIRONMENT,
+            environment: process.env.NEXT_DATOCMS_ENVIRONMENT || null,
           }
         : {
             enabled: false,
