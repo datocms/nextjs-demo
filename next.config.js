799a98de632464b36e0cb65ee10254cab40946cc
Stefano Verna <s.verna@datocms.com>
2021-04-20 13:01:08 +0200

    Update Next and react-datocms. Use webpack 5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
 module.exports = {
+  future: {
+    webpack5: true,
+  },
   env: {
     NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN:
       process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
