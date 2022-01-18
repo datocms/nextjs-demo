7cddc6fb084681121566c1166028ddc97f83a3ec
Thomas Verleye <thomasverleye@live.be>
2022-01-18 15:10:12 +0100

    `preview` is undefined in `Post`
    This is a bug which makes it impossible to test the real time preview mode on blog posts.

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -105,6 +105,7 @@ export async function getStaticProps({ params, preview = false }) {
             enabled: false,
             initialData: await request(graphqlRequest),
           },
+      preview,
     },
   };
 }
