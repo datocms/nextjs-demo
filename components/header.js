5d4387f11644c013e5f4748c61e8cac94964134a
Pierre <71701076+stephanepierre@users.noreply.github.com>
2022-10-17 17:04:48 +0200

    Update header.js

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/">
-        <a className="hover:underline">Blog</a>
+        <a className="hover:underline">Blog TEST avec DatoCMS</a>
       </Link>
       .
     </h2>
